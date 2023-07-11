62eb0a94a6596f3453bf5d3e2f6565981e33709b
Shouvik Paul <shouvik.paul@ah.nl>
2023-07-11 17:08:17 +0200

    add method level security

--- src/main/java/com/shvk/security/config/SecurityConfig.java
+++ src/main/java/com/shvk/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.shvk.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +15,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -22,6 +24,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    //authorization
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable) // replaced lamda csrf -> csrf.disable()
@@ -31,6 +34,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    //authentication
     public UserDetailsService userDetailsService() {
         UserDetails user = User.builder()
             .username("user")
